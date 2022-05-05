7c1e5bf246c4e9efce56ce4c0536cdb40efa5375
akramHoque <96454691+akramHoque@users.noreply.github.com>
2022-05-05 22:51:09 +0600

    add new items

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -19,9 +19,23 @@ const MyItems = () => {
 
     }, [user])
     return (
-        <div>
-            <h2>This is my Items : {item.length}</h2>
+       <div className='container'>
+            <div className='row'>
+            <h2 className='text-center mb-3'><span className='text-secondary'>This is my Items</span></h2>
+            <div className='items-container mb-5'>
+            {
+                item.map(order => 
+                    <div className='item' key = {order?._id}>
+                        <img className='w-100' src={order.img} alt="" />
+                        <h4>{order.name}</h4>
+                        <p>{order.price}</p>
+                    </div>
+                    )
+            }
+            </div>
+           
         </div>
+       </div>
     );
 };
 
